38de7c99e85a50068a225f0ad89f911015340d3d
Sávio Cunha <60552629+saviocunha2@users.noreply.github.com>
2022-09-13 13:04:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const imagem = fetch('https://viacep.com.br/ws/29280000/json/');
+
+imagem.then(r =>{
+    const r2 = r.clone();
+    r.text().then((text) =>{
+        console.log(text);
+    })
+    r2.json().then((json)=>{
+        console.log(json);
+    })
+    console.log(r);
+})
+.then(body =>{
+    console.log(body);
+});
